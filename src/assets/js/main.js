f42aca7a7cf57179659a44de82fe8f606c0eac79
DukeBJ <bjworld@gmail.com>
2021-01-29 02:01:50 +0300

    ups front

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,10 @@
 import html2canvas from 'html2canvas';
 
-html2canvas(document.querySelector("#capture")).then(canvas => {
-  document.body.appendChild(canvas);
-  const cnvs = document.querySelector('canvas');
+// html2canvas(document.querySelector("#capture")).then(canvas => {
+//   document.body.appendChild(canvas);
+//   const cnvs = document.querySelector('canvas');
 
-  const link = document.getElementById('link');
-  link.setAttribute('download', 'MintyPaper.png');
-  link.setAttribute('href', cnvs.toDataURL("image/png").replace("image/png", "image/octet-stream"));
-});
+//   const link = document.getElementById('link');
+//   link.setAttribute('download', 'MintyPaper.png');
+//   link.setAttribute('href', cnvs.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+// });
